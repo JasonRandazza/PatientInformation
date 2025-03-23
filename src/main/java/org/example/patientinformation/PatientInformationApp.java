0492a315b4990a93c797457f754ab1774b78ccf8
JasonRandazza <135364916+JasonRandazza@users.noreply.github.com>
2025-03-23 15:28:43 -0400

    Renamed to PatientInformationApp from HelloApplication. Firebase connectivity added to start method.

--- src/main/java/org/example/patientinformation/PatientInformationApp.java
+++ src/main/java/org/example/patientinformation/PatientInformationApp.java
@@ -1,5 +1,6 @@
 package org.example.patientinformation;
 
+import com.google.cloud.firestore.Firestore;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,10 +8,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class PatientInformationApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FirestoreContext.initialize();
+        Firestore db = FirestoreContext.getDB();
+        FXMLLoader fxmlLoader = new FXMLLoader(PatientInformationApp.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
