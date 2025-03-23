b8a33c0d4e26bd0bb271e6cd82ba1000f1d032c3
JasonRandazza <135364916+JasonRandazza@users.noreply.github.com>
2025-03-23 15:31:27 -0400

    Google & Firebase packages added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,12 @@ module org.example.patientinformation {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires firebase.admin;
+    requires com.google.auth;
+    requires com.google.auth.oauth2;
+    requires google.cloud.firestore;
+    requires google.cloud.core;
+    requires com.google.api.apicommon;
 
     opens org.example.patientinformation to javafx.fxml;
     exports org.example.patientinformation;
