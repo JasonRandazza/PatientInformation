282463b30c0c000d9809b6922a2da6a2c53df634
umerkamran8@gmail.com <NewYork@44>
2025-04-03 23:09:57 -0400

    I implemented the email and password field prompt as well as sign in and regsiter buttons.

--- src/main/java/org/example/patientinformation/PatientLoginScreenController.java
+++ src/main/java/org/example/patientinformation/PatientLoginScreenController.java
@@ -1,5 +1,28 @@
 package org.example.patientinformation;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.event.ActionEvent;
+
 public class PatientLoginScreenController {
 
+    @FXML private TextField emailField;
+    @FXML private PasswordField passwordField;
+
+    @FXML
+    private void handleSignIn(ActionEvent event) {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        System.out.println("Sign In clicked. Email: " + email + ", Password: " + password);
+        // TODO: Call AuthService to verify credentials
+    }
+
+    @FXML
+    private void handleRegister(ActionEvent event) {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        System.out.println("Register clicked. Email: " + email + ", Password: " + password);
+        // TODO: Call AuthService to register new user
+    }
 }
