7019d19de45fa9cae821644e5ea6f8f30a9794b1
umerkamran8@gmail.com <NewYork@44>
2025-04-03 23:18:15 -0400

    I implemented the email and password field prompt as well as sign in , register and go back button. Functionality of it does not work just yet

--- src/main/java/org/example/patientinformation/PatientLoginScreenController.java
+++ src/main/java/org/example/patientinformation/PatientLoginScreenController.java
@@ -1,28 +1,60 @@
 package org.example.patientinformation;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+import java.io.IOException;
 
 public class PatientLoginScreenController {
 
-    @FXML private TextField emailField;
-    @FXML private PasswordField passwordField;
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
 
     @FXML
     private void handleSignIn(ActionEvent event) {
         String email = emailField.getText();
         String password = passwordField.getText();
+
+        // Basic debug message
         System.out.println("Sign In clicked. Email: " + email + ", Password: " + password);
-        // TODO: Call AuthService to verify credentials
+
+        // TODO: Add sign-in logic here (e.g., AuthService.login(email, password))
     }
 
     @FXML
     private void handleRegister(ActionEvent event) {
         String email = emailField.getText();
         String password = passwordField.getText();
+
+        // Basic debug message
         System.out.println("Register clicked. Email: " + email + ", Password: " + password);
-        // TODO: Call AuthService to register new user
+
+        // TODO: Add registration logic here (e.g., AuthService.register(email, password))
+    }
+
+    @FXML
+    private void handleGoBack(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/startmenu.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Hospital App - Start Menu");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
